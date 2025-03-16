4da608c0d8840ad0c1332ce5a89cce3b20432dbf
Matheus-Malara <matheus_malara@live.com>
2025-03-16 04:50:22 -0300

    Solution problem 58

---
+++ src/ep58lengthoflastword/Main.java
@@ -0,0 +1,9 @@
+package ep58lengthoflastword;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LenghtOfLastWordSolution.lengthOfLastWord("Hello World"));
+        System.out.println(LenghtOfLastWordSolution.lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(LenghtOfLastWordSolution.lengthOfLastWord("luffy is still joyboy"));
+    }
+}
