6e08a38542d6cf3b56693bdc0f0013458423383c
Matheus-Malara <matheus_malara@live.com>
2025-03-16 22:30:46 -0300

    Solution problem 66

---
+++ src/ep66plusone/Main.java
@@ -0,0 +1,12 @@
+package ep66plusone;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(PlusOneSolution.plusOne(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(PlusOneSolution.plusOne(new int[]{4, 3, 2, 1})));
+        System.out.println(Arrays.toString(PlusOneSolution.plusOne(new int[]{9})));
+        System.out.println(Arrays.toString(PlusOneSolution.plusOne(new int[]{9, 9, 9, 9})));
+    }
+}
