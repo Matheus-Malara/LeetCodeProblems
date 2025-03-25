f231a47e99a7b57184035bc3c5c4349f2b035e56
matheus <matheus.malara@delivoro.com.br>
2025-03-24 21:50:47 -0300

    Solution problem 191

---
+++ src/ep191numberof1bits/Main.java
@@ -0,0 +1,10 @@
+package ep191numberof1bits;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(NumberOf1BitsSolution.hammingWeight(15));
+        System.out.println(NumberOf1BitsSolution.hammingWeight(11));
+        System.out.println(NumberOf1BitsSolution.hammingWeight(128));
+        System.out.println(NumberOf1BitsSolution.hammingWeight(2147483645));
+    }
+}
