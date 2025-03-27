21e449ecd9d47dfec20f782ad2c871ab4438ed91
Matheus-Malara <matheus_malara@live.com>
2025-03-26 21:37:47 -0300

    Solution problem 217

---
+++ src/ep217containsduplicate/Main.java
@@ -0,0 +1,10 @@
+package ep217containsduplicate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ContainsDuplicateSolution.containsDuplicate(new int[]{1, 2, 3, 1}));
+        System.out.println(ContainsDuplicateSolution.containsDuplicate(new int[]{1, 2, 3, 4}));
+        System.out.println(ContainsDuplicateSolution.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        System.out.println(ContainsDuplicateSolution.containsDuplicate(new int[]{1, 2, 3, 1}));
+    }
+}
