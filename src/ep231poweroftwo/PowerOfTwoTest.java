7635b109e56b1039c193352598feff95507aaede
Matheus-Malara <matheus_malara@live.com>
2025-04-03 19:17:20 -0300

    Fix problem 231

--- src/ep231poweroftwo/PowerOfTwoTest.java
+++ src/ep231poweroftwo/PowerOfTwoTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PowerOfTwoSolutionTest {
+class PowerOfTwoTest {
 
     @Test
     void testIsPowerOfTwoWithPositivePowerOfTwo() {
